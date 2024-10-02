5980a26531dd98849613c086562746a5808cabee
Daria <dariaasieieva@gmail.com>
2024-10-02 16:42:04 +0200

    1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+`use strict`
+const elements = document.querySelectorAll('#categories .item');
+
+
+console.log(`Number of categories: ${elements.length}`);
+
+elements.forEach(element => {
+    const elementName = element.querySelector('h2');
+    console.log(`Category: ${elementName.textContent}`);
+
+    const elementQuantity = element.querySelectorAll('ul li');
+    console.log(`Elemets: ${elementQuantity.length}`);
+});
+
+
+
+
